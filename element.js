ce33ab813356b4da9c872828dce4d0e965bd1bcc
David Pitman <david.pitman@cloudouble.com>
2023-03-13 14:36:42 +1000

    refactoring for initial undocumented release

--- element.js
+++ element.js
@@ -82,7 +82,7 @@ const Element = Object.defineProperties({}, {
         for (const a in source.getAttributeNames()) {
             if (!keep.includes(a) && !((autoKeepB37) && a.startsWith('b37-'))) continue
             const aValue = source.getAttribute(a)
-            aValue === '' ? target.toggleAttribute(a, true) : aValue && target.setAttribute(a, aValue)
+            (aValue && target.setAttribute(a, aValue)) || (aValue === '' && target.toggleAttribute(a, true))
         }
     }},
     stackTemplates: {configurable: false, enumerable: true, writable: false, value: async function(tagId, templateInnerHTML=undefined) {
