b2b574e14038baf41731b81e445d9b804ed7fdde
David Pitman <david.pitman@cloudouble.com>
2023-03-29 16:49:08 +1000

    refactoring for initial undocumented release

--- element.js
+++ element.js
@@ -64,9 +64,6 @@ const Element = Object.defineProperties({}, {
         }
         element.getAttribute(`b37-source`) && parseb37source(element.getAttribute('b37-source'), element.b37Dataset)
         element.getAttribute(`b37-source-${event.type}`) && parseb37source(element.getAttribute(`b37-source-${event.type}`), element.b37Dataset)
-        element.b37Source && typeof element.b37Source === 'object' && event.type in element.b37Source && parseb37source(element.b37Source[event.type],
-                element.b37Dataset)
-        event.b37Source && parseb37source(event.b37Source, event)
         if (event.formData || event.detail || event.data) {
             Object.assign(processorData, (event.formData instanceof Object && event.formData) || (event.detail instanceof Object && event.detail)
                 || (event.data instanceof Object && event.data) || {})
@@ -91,9 +88,6 @@ const Element = Object.defineProperties({}, {
         } else if (event.b37Sink && typeof event.b37Sink === 'object') {
             event.b37Sink = (event.b37Sink && typeof event.b37Sink === 'object')
                 ? Object.assign(event.b37Sink, processorData) : processorData
-        } else if (element.b37Sink && typeof element.b37Sink === 'object' && event.type in element.b37Sink) {
-            element.b37Sink[event.type] = (element.b37Sink[event.type] && typeof element.b37Sink[event.type] === 'object')
-                ? Object.assign(element.b37Sink[event.type], processorData) : processorData
         } else if (element.getAttribute(`b37-sink-${event.type}`)) {
             const b37Sink = element.getAttribute(`b37-sink-${event.type}`)
             element.b37Sink[b37Sink] = (element.b37Sink[b37Sink] && typeof element.b37Sink[b37Sink] === 'object') 
