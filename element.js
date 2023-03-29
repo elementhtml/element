a74cc0de7fe39e3fd982644bc242508ee6262973
David Pitman <david.pitman@cloudouble.com>
2023-03-29 20:44:13 +1000

    refactoring for initial undocumented release

--- element.js
+++ element.js
@@ -177,12 +177,9 @@ const Element = Object.defineProperties({}, {
         if (!document.head.querySelector('meta[name="generator"][content="Element"]')) return
         const eContentNode = document.head.querySelector('meta[name="e-content"][content]'), 
             eTemplateNode = document.head.querySelector('meta[name="e-template"][content]'), documentTemplate = eTemplateNode?.getAttribute('content')
-        const [contentRepo='content', contentMode='/'] = (eContentNode?.getAttribute('content')||'').split('-')
-        if (contentMode === '/') {
-            const contentBase = `${((new URL(eContentNode.dataset.base || document.location.pathname, document.location)).href).split('/').slice(0,-1).join('/')}/`
-            console.log('line 183', contentBase)
-        }
-
+        const [contentRepo='content', contentMode='/'] = (eContentNode?.getAttribute('content')||'').split('-'), 
+            contentBase = `${((new URL(eContentNode.dataset.base || document.location.pathname, document.location)).href).split('/').slice(0,-1).join('/')}/`, 
+            contentSuffix = this.repos[contentRepo]?.content?.suffix || 'md'
 
 
 
