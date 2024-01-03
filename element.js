a4e7f47e8013ed0eaced7bc33c724a9418b279ed
David Pitman <david.pitman@cloudouble.com>
2024-01-03 19:29:19 +1000

    initial beta release

--- element.js
+++ element.js
@@ -1,6 +1,6 @@
 const ElementHTML = Object.defineProperties({}, {
 
-    version: { enumerable: true, value: '0.9.9' },
+    version: { enumerable: true, value: '1.0.0' },
 
     sys: {
         enumerable: false, value: Object.freeze({
