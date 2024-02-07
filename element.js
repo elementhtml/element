3b0f1794a305d1ac413d85c943a0ed2063760ec3
David Pitman <david.pitman@cloudouble.com>
2024-02-07 13:42:21 +1000

    beta minor fixes

--- element.js
+++ element.js
@@ -838,7 +838,8 @@ const ElementHTML = Object.defineProperties({}, {
     getTagId: {
         value: function (tag) {
             if (this.ids[tag]) return this.ids[tag]
-            const [namespace, pointer] = tag.split('-', 2).map(t => t.toLowerCase())
+            let [namespace, ...pointer] = tag.split('-').map(t => t.toLowerCase()).filter(s => !!s)
+            pointer = pointer.join('/')
             if (namespace === 'e') return (new URL(`./e/components/${pointer}.html`, import.meta.url)).href
             if (this.env.namespaces[namespace]) return (new URL(`${this.env.namespaces[namespace]}/${pointer}.html`, document.baseURI)).href
             return (new URL(`${namespace}/${pointer}.html`, document.baseURI)).href
