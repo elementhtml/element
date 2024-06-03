1a204fd07d3e71fd913dd1c64333c417a128f5b0
David Pitman <david.pitman@cloudouble.com>
2024-06-03 12:00:36 +1000

    optimizing for next major release

---
+++ modules/compile.js
@@ -0,0 +1,228 @@
+const module = {
+
+    compileFacet: {
+        enumerable: true,
+        value: async function (directives, hash) {
+            hash ??= await this.digest(directives = this.canonicalizeDirectives(directives))
+            const fieldNames = new Set(), cellNames = new Set(), statements = []
+            let statementIndex = -1
+            for (let directive of directives.split(this.sys.regexp.splitter)) {
+                statementIndex = statementIndex + 1
+                const statement = { labels: new Set(), steps: [] }
+                let stepIndex = -1
+                for (let [index, segment] of directive.split(' >> ').entries()) {
+                    segment = segment.trim()
+                    if (!segment) continue
+                    let handlerExpression = segment, label, defaultExpression, hasDefault = false
+                    const step = {}, labelMatch = handlerExpression.match(this.sys.regexp.label)
+                    if (labelMatch) {
+                        label = labelMatch[1].trim()
+                        handlerExpression = handlerExpression.slice(labelMatch[0].length).trim()
+                    }
+                    const defaultExpressionMatch = handlerExpression.match(this.sys.regexp.defaultValue)
+                    if (defaultExpressionMatch) {
+                        defaultExpression = defaultExpressionMatch[1].trim()
+                        handlerExpression = handlerExpression.slice(0, defaultExpressionMatch.index).trim()
+                        hasDefault = !!defaultExpression
+                        if (defaultExpression[0] === '#') {
+                            const cn = defaultExpression.slice(1).trim()
+                            if (cn) cellNames.add(cn)
+                        }
+                    }
+                    label ||= `${index}`
+                    const labelModeFlag = label[label.length - 1], labelMode = labelModeFlag === '!' ? 'force' : ((labelModeFlag === '?') ? 'silent' : undefined)
+                    if (labelMode) {
+                        label = label.slice(0, -1).trim()
+                        step.labelMode = labelMode
+                    }
+                    step.label = label
+                    switch (label[0]) {
+                        case '@':
+                            let fn = label.slice(1).trim()
+                            if (fn) fieldNames.add(fn)
+                            break
+                        case '#':
+                            const cn = label.slice(1).trim()
+                            if (cn) cellNames.add(cn)
+                            break
+                        default:
+                            const ln = label.trim()
+                            if (ln) statement.labels.add(ln)
+                    }
+                    let params
+                    stepIndex = stepIndex + 1
+                    switch (handlerExpression) {
+                        case '#': case '?': case '/': case ':':
+                            params = this.parsers.router(handlerExpression, hasDefault)
+                            break
+                        default:
+                            switch (handlerExpression[0]) {
+                                case '`':
+                                    params = this.parsers.proxy(handlerExpression.slice(1, -1), hasDefault)
+                                    break
+                                case '/':
+                                    params = this.parsers.pattern(handlerExpression.slice(1, -1), hasDefault)
+                                    break
+                                case '"': case "'":
+                                    params = this.parsers.string(handlerExpression.slice(1, -1), hasDefault)
+                                    break
+                                case "#": case "@":
+                                    params = this.parsers.state(handlerExpression, hasDefault)
+                                    for (const addedName of (params.ctx.vars.addedFieldNames ?? [])) fieldNames.add(addedName)
+                                    for (const addedName of (params.ctx.vars.addedCellNames ?? [])) cellNames.add(addedName)
+                                    break
+                                case "$":
+                                    if (handlerExpression[1] === "{") {
+                                        params = this.parsers.variable(handlerExpression, hasDefault)
+                                    } else if (handlerExpression[1] === "(") {
+                                        params = this.parsers.selector(handlerExpression.slice(2, -1), hasDefault)
+                                    }
+                                    break
+                                case "(":
+                                    params = this.parsers.transform(handlerExpression, hasDefault)
+                                    break
+                                case "{": case "[":
+                                    params = this.parsers.json(handlerExpression, hasDefault)
+                                    break
+                                case "n": case "t": case "f": case "0": case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "7": case "9":
+                                    let t
+                                    switch (handlerExpression) {
+                                        case 'null': case 'true': case 'false':
+                                            t = true
+                                        default:
+                                            if (t || handlerExpression.match(this.sys.regexp.isNumeric)) params = this.parsers.json(handlerExpression, hasDefault)
+                                    }
+                                    break
+                                case "_":
+                                    if (handlerExpression.endsWith('_')) {
+                                        params = this.parsers.wait(handlerExpression.slice(1, -1), hasDefault)
+                                        break
+                                    }
+                                case '~':
+                                    if (handlerExpression.endsWith('~')) handlerExpression = handlerExpression.slice(1, -1)
+                                default:
+                                    params = this.parsers.network(handlerExpression, hasDefault)
+                            }
+                    }
+                    step.params = params
+                    if (defaultExpression) step.defaultExpression = defaultExpression
+                    statement.labels.add(label)
+                    statement.labels.add(`${index}`)
+                    statement.labels = Array.from(statement.labels)
+                    statement.steps.push(Object.freeze(step))
+                }
+                Object.seal(statement.labels)
+                Object.freeze(statement.steps)
+                Object.freeze(statement)
+                statements.push(statement)
+            }
+            const FacetClass = class extends this.Facet {
+                static fieldNames = Array.from(fieldNames)
+                static cellNames = Array.from(cellNames)
+                static statements = statements
+                static hash = hash
+            }
+            FacetClass.E = this
+            return FacetClass
+        }
+    },
+
+    parsers: {
+        value: {
+            json: function (expression, hasDefault) {
+                let value = null
+                try { value = JSON.parse(expression) } catch (e) { }
+                return { handler: 'json', ctx: { vars: { value } } }
+            },
+            network: function (expression, hasDefault) {
+                const expressionIncludesValueAsVariable = (expression.includes('${}') || expression.includes('${$}'))
+                let returnFullRequest = false
+                if (expression[0] === '~' && expression.endsWith('~')) {
+                    returnFullRequest = true
+                    expression = expression.slice(1, -1)
+                }
+                return { handler: 'network', ctx: { vars: { expression, expressionIncludesValueAsVariable, hasDefault, returnFullRequest } } }
+            },
+            pattern: function (expression, hasDefault) {
+                expression = expression.trim()
+                if (!expression) return
+                return { handler: 'pattern', ctx: { binder: true, vars: { expression, regexp: this.env.regexp[expression] ?? new RegExp(expression) } } }
+            },
+            proxy: function (expression, hasDefault) {
+                const [parentExpression, childExpression] = expression.split('.').map(s => s.trim())
+                if (!parentExpression || (childExpression === '')) return
+                let [parentObjectName, ...parentArgs] = parentExpression.split('(').map(s => s.trim())
+                parentArgs = parentArgs.join('(').slice(0, -1).trim().split(',').map(s => s.trim())
+                let useHelper = parentObjectName[0] === '~', childMethodName, childArgs
+                if (useHelper) {
+                    parentObjectName = parentObjectName.slice(1)
+                } else {
+                    [childMethodName, ...childArgs] = childExpression.split('(').map(s => s.trim())
+                    childArgs = childArgs.join('(').slice(0, -1).trim().split(',').map(s => s.trim())
+                }
+                return { handler: 'proxy', ctx: { binder: true, vars: { childArgs, childMethodName, parentArgs, parentObjectName, useHelper } } }
+            },
+            router: function (expression, hasDefault) {
+                let handler
+                switch (expression) {
+                    case '#':
+                        return { handler: 'routerhash', ctx: { binder: true, vars: { signal: true } } }
+                        break
+                    case '?':
+                        handler = 'routersearch'
+                        break
+                    case '/':
+                        handler = 'routerpathname'
+                        break
+                    case ':':
+                        handler = 'router'
+                }
+                return { handler }
+            },
+            selector: function (expression, hasDefault) {
+                if (!expression.includes('|')) {
+                    switch (expression[0]) {
+                        case '#':
+                            expression = `:document|${expression}`
+                            break
+                        case '@':
+                            expression = `:root|[name="${expression.slice(1)}"]`
+                            break
+                        case '^':
+                            expression = `:root|[style~="${expression.slice(1)}"]`
+                            break
+                        case '~':
+                            expression = `:root|[itemscope] [itemprop="${expression.slice(1)}"]`
+                            break
+                        case '.':
+                        default:
+                            expression = `:root|${expression}`
+                    }
+                }
+                const [scopeStatement, selectorStatement] = expression.split('|').map(s => s.trim())
+                return { handler: 'selector', ctx: { binder: true, vars: { scopeStatement, selectorStatement, signal: true } } }
+            },
+            state: function (expression, hasDefault) {
+                return { handler: 'state', ctx: { binder: true, vars: { expression: expression.slice(1), signal: true, typeDefault: expression[0] } } }
+            },
+            string: function (expression, hasDefault) {
+                return { handler: 'string', ctx: { vars: { expression } } }
+            },
+            transform: function (expression, hasDefault) {
+                if (expression && expression.startsWith('(`') && expression.endsWith('`)')) expression = expression.slice(1, -1)
+                if (expression.startsWith('`~/')) expression = '`transforms' + expression.slice(2)
+                if (expression.endsWith('.`')) expression = expression.slice(0, -1) + 'jsonata`'
+                return { handler: 'transform', ctx: { vars: { expression } } }
+            },
+            variable: function (expression, hasDefault) {
+                return { handler: 'variable', ctx: { vars: { expression } } }
+            },
+            wait: function (expression, hasDefault) {
+                return { handler: 'wait', ctx: { vars: { expression } } }
+            }
+        }
+    }
+
+}
+
+export { module }
---
+++ modules/dev.js
@@ -0,0 +1,252 @@
+const module = {
+
+    exportComponent: {
+        enumerable: true,
+        value: async function (id, format = 'plain') {
+            if (id instanceof HTMLElement) id = id instanceof this.Component ? id.constructor.id : (this.app.components.instances.get(id)?.constructor?.id)
+            const componentManifest = {
+                id, extends: this.extends[id] ?? 'HTMLElement',
+                style: this.styles[id] ?? '',
+                template: this.templates[id] ?? '',
+                class: this.classes[id]
+            }
+            switch (format) {
+                case 'json': case 'xdr':
+                    if (componentManifest.style instanceof HTMLStyleElement) componentManifest.style = componentManifest.style.textContent
+                    if (componentManifest.template instanceof HTMLTemplateElement) componentManifest.template = componentManifest.template.content.cloneNode(true).textContent
+                    if (componentManifest.class instanceof Function) componentManifest.class = componentManifest.class.toString()
+                    if (format === 'json') return JSON.stringify(componentManifest)
+                    await this.loadHelper('xdr')
+                    return this.useHelper('xdr', 'stringify', componentManifest, await this.getXdrType('component'))
+                default:
+                    return componentManifest
+            }
+        }
+    },
+
+    exportFacet: {
+        enumerable: true,
+        value: async function (source, format = 'plain') {
+            const facetManifest = { fieldNames: [], cellNames: [], statements: [], hash: undefined }
+            if (!source) return facetManifest
+            let facetClass
+            switch (typeof source) {
+                case 'string':
+                    facetClass = this.app.facets.classes[source] ?? this.env.facets[source]
+                case 'function':
+                    facetClass ??= source
+                case 'object':
+                    if (source instanceof HTMLElement) facetClass ??= this.app.facets.instances.get(source)?.constructor
+                    if (facetClass) for (const p in facetManifest) facetManifest[p] = facetClass[p]
+            }
+            switch (format) {
+                case 'json': case 'xdr':
+                    if (format === 'json') return JSON.stringify(facetManifest)
+                    await this.loadHelper('xdr')
+                    return this.useHelper('xdr', 'stringify', facetManifest, await this.getXdrType('facet'))
+                default:
+                    return facetManifest
+            }
+        }
+    },
+
+    exportPackage: {
+        enumerable: true,
+        value: async function (includePackages, includeComponents, includeFacets) {
+            includePackages ??= new Set()
+            includeComponents ??= new Set()
+            includeFacets ??= new Set()
+            const openingLine = 'const Package = {}', closingLine = 'export default Package', packageChunks = [], packageUrls = [], appPackages = this.app.packages ?? new Map()
+            if (Array.isArray(includePackages)) {
+                for (const packageKey of includePackages) if (appPackages.has(packageKey)) packageUrls.push(appPackages.get(packageKey))
+            } else if (includePackages instanceof Set) {
+                for (const [packageKey, packageUrl] of appPackages.entries()) if (!includePackages.has(packageKey)) packageUrls.push(packageUrl)
+            }
+            for (const packageUrl of packageUrls) packageChunks.push((await (await fetch(packageUrl)).text()).trim().split('\n')
+                .filter(l => !(l.trim().startsWith(openingLine) || l.trim().startsWith(closingLine))).join('\n').trim())
+            for (const [lc, uc] of [['component', 'Component'], ['facet', 'Facet']]) {
+                const m = new Map(), include = lc === 'component' ? includeComponents : includeFacets
+                if (Array.isArray(include)) {
+                    for (const id of include) if (this.env[lc][id]) m.set(id, this.env[lc][id])
+                } else if (include instanceof Set) {
+                    for (const id in this.env[lc]) if (!include.has(id)) m.set(id, this.env[lc][id])
+                }
+                if (m.size) {
+                    packageChunks.push(`package.${lc}s ??= {}`)
+                    for (const [id, manifest] of m.entries()) packageChunks.push(`package.${lc}s['${id}'] = ${await this[`export${uc}`](id, 'json')}`)
+                }
+            }
+            packageChunks.push('export default Package')
+            return packageChunks.join('/n/n')
+        }
+    },
+
+    exportApplication: {
+        enumerable: true,
+        value: async function (manifest) {
+            const { source, priority, name, author, title, description, icon, links, meta, og, cards, schema, dc, blocks, pwa, robots, sitemap, assets, targets, vars } = manifest
+            const metaTypesConfig = { og: { nameAttr: 'property', namePrefix: 'og' }, cards: { namePrefix: 'twitter' }, dc: { namePrefix: 'dc' } }
+            const sitemapEntries = sitemap === true ? [] : (Array.isArray(sitemap) ? [...sitemap] : undefined)
+            const application = {}
+            if (targets[0] !== 'index.html') targets.unshift('index.html')
+            if (targets[0] === '/') targets[0] === 'index.html'
+
+            for (let target of targets) {
+                if (typeof target === 'string') target = { path: target }
+                const template = document.createElement('template')
+                template.innerHTML = await (await fetch(target.source ?? source)).text()
+                let titleElement = template.content.querySelector('head > title'), markerElement = titleElement
+                if (!'title' in target) target.title = title
+                if (target.title) titleElement.textContent = target.title
+                const targetLinks = target.links ?? JSON.parse(JSON.stringify(links))
+                if (icon && !targetLinks.icon) targetLinks.icon = icon
+                if (pwa && !targetLinks.manifest) targetLinks.manifest = 'manifest.json'
+                if (sitemap && !targetLinks.sitemap) targetLinks.sitemap = 'sitemap.xml'
+                for (const link in targetLinks) {
+                    const linkAttrs = link && typeof link === 'object' ? link : { rel: link, href: targetLinks[link] }
+                    if (!linkAttrs.rel) continue
+                    const linkElement = template.content.querySelector(`head > link[rel="${linkAttrs.rel}"]`) ?? markerElement.insertAdjacentElement(document.createElement('link'))
+                    for (const n in linkAttrs) linkElement.setAttribute(n, linkAttrs[n])
+                    markerElement = linkElement
+                }
+                const targetMeta = target.meta ?? JSON.parse(JSON.stringify(meta))
+                if (pwa && !targetMeta['application-name']) targetMeta['application-name'] = target.name ?? name ?? target.title
+                if (!targetMeta.author && author) targetMeta.author = author
+                if (!targetMeta.description && description) targetMeta.description = description
+                for (const m in targetMeta) {
+                    const metaAttrs = m && typeof m === 'object' ? m : { name: m, content: targetMeta[m] }
+                    if (!metaAttrs.name) continue
+                    const metaElement = template.content.querySelector(`head > meta[name="${metaAttrs.name}"]`) ?? markerElement.insertAdjacentElement(document.createElement('meta'))
+                    for (const n in metaAttrs) metaElement.setAttribute(n, metaAttrs[n])
+                    markerElement = metaElement
+                }
+                for (let [sourceType, typeMeta] of Object.entries({
+                    og: target.og ?? (og ? JSON.parse(JSON.stringify(og)) : undefined),
+                    cards: target.cards ?? (cards ? JSON.parse(JSON.stringify(cards)) : undefined),
+                    dc: target.dc ?? (dc ? JSON.parse(JSON.stringify(dc)) : undefined)
+                })) {
+                    if (!typeMeta) continue
+                    if (typeMeta === true) typeMeta = {}
+                    typeMeta.title ??= targetMeta.title ?? target.title
+                    typeMeta.description ??= targetMeta.description ?? target.description
+                    typeMeta.image ??= targetLinks.icon
+                    if (sourceType === 'cards') typeMeta.card ??= 'summary'
+                    const nameAttr = metaTypesConfig[sourceType].nameAttr ?? 'name', namePrefix = metaTypesConfig[sourceType].namePrefix
+                    for (const m in typeMeta) {
+                        const metaAttrs = m && typeof m === 'object' ? m : { [`${namePrefix}:${m}`]: m, content: typeMeta[m] }
+                        if (!metaAttrs[nameAttr] || !metaAttrs.content) continue
+                        const nameValue = metaAttrs[nameAttr].startsWith(`${namePrefix}:`) ? metaAttrs[nameAttr] : `${namePrefix}:${metaAttrs[nameAttr]}`
+                        const metaElement = template.content.querySelector(`head > meta[${nameAttr}="${nameValue}"]`) ?? markerElement.insertAdjacentElement(document.createElement('meta'))
+                        for (const n in metaAttrs) if (n !== 'content' && n !== nameAttr) metaElement.setAttribute(n, metaAttrs[n])
+                        metaElement.setAttribute(nameAttr, nameValue)
+                        metaElement.setAttribute('content', metaAttrs.content)
+                        markerElement = metaElement
+                    }
+                }
+                const targetSchema = target.schema ?? (schema ? JSON.parse(JSON.stringify(schema)) : undefined)
+                if (targetSchema) {
+                    targetSchema['@context'] ??= 'https://schema.org'
+                    targetSchema['@type'] ??= 'WebSite'
+                    targetSchema.headline ??= targetMeta.title
+                    targetSchema.image ??= [targetLinks.icon]
+                    const targetAuthor = targetMeta.author ?? target.author ?? author
+                    if (targetAuthor) targetSchema.author ??= { '@type': 'Person', name: targetAuthor }
+                    const schemaElement = template.content.querySelector(`head > script[type="application/ld+json"]`) ?? markerElement.insertAdjacentElement(document.createElement('script'))
+                    schemaElement.setAttribute('type', 'application/ld+json')
+                    schemaElement.textContent = JSON.stringify(targetSchema, null, 4)
+                    markerElement = schemaElement
+                }
+                let targetBlocks = target.blocks ?? blocks ?? []
+                targetBlocks = targetBlocks.filter(b => !b.global)
+                if (blocks) targetBlocks = JSON.parse(JSON.stringify([...(blocks.filter(b => b.global)), ...targetBlocks]))
+                for (let targetBlock of targetBlocks) {
+                    if (typeof targetBlock === 'string') { targetBlock = { code: targetBlock } }
+                    const { code, position, selector } = targetBlock
+                    if (!code) continue
+                    const blockMarker = selector ? template.content.querySelector(selector) : markerElement
+                    if (!blockMarker) continue
+                    blockMarker.insertAdjacentHTML(position ?? (selector ? 'beforeend' : 'afterend'), code)
+                }
+                let targetPath = path ? `${path}/${target.path}` : target.path
+                if (targetPath.endsWith('/')) targetPath = `${targetPath}index.html`
+                if (targetPath.startsWith('/')) targetPath = targetPath.slice(1)
+                if (!targetPath.includes('.')) targetPath = `${targetPath}.html`
+                application[targetPath] = {
+                    content: template.content.querySelector('html').outerHTML,
+                    contentType: 'text/html'
+                }
+                target.url ??= `https://${host}/${targetPath}`
+                if (sitemap === true) sitemapEntries.push({ loc: `https://${host}/${targetPath}`, priority: target.priority ?? priority })
+            }
+
+            if (pwa) {
+                if (pwa === true) pwa = {}
+                pwa.name ??= name ?? title
+                pwa.short_name ??= title ?? name
+                pwa.description ??= description
+                pwa.icons ??= [{ src: `/${icon}`, sizes: '512x512', type: `image/${icon.split('.').pop()}` }]
+                pwa.start_url ??= path ? `${path}/${targets[0]}` : `/${targets[0]}`
+                pwa.display ??= 'standalone'
+                pwa.background_color ??= '#ffffff'
+                pwa.theme_color ??= '#000000'
+                pwa.orientation ??= 'portrait-primary'
+                application['manifest.json'] = {
+                    content: JSON.stringify(pwa, null, 4),
+                    contentType: 'application/json'
+                }
+            }
+
+            if (robots) {
+                let robotsContent = ''
+                if (typeof robots === 'string') {
+                    robotsContent = robots
+                } else {
+                    if (robots === true) robots = {}
+                    robots['User-agent'] ??= '*'
+                    robots.Allow ??= ['/']
+                    robots.Disallow ??= []
+                    for (const d in robots) {
+                        switch (d) {
+                            case 'Allow': case 'Disallow':
+                                for (const dd of robots[d]) robotsContent += `${d}: ${dd}\n`
+                                break
+                            default:
+                                robotsContent += `${d}: ${robots[d]}\n`
+                        }
+                    }
+                }
+                application['robots.txt'] = {
+                    content: robotsContent,
+                    contentType: 'text/plain'
+                }
+            }
+
+            if (sitemap) {
+                let sitemapContent = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+                for (const entry of sitemapEntries) {
+                    sitemapContent += `<url><loc>${entry.loc}</loc><priority>${entry.priority}</priority></url>\n`
+                }
+                sitemapContent += '</urlset>'
+                application['sitemap.xml'] = {
+                    content: sitemapContent,
+                    contentType: 'application/xml'
+                }
+            }
+
+            for (const asset of (assets ?? [])) application[asset.path] = { content: { '/': asset.path }, contentType: asset.contentType }
+
+            return application
+        }
+    },
+
+    publishApplication: {
+        enumerable: true,
+        value: async function (application, target) {
+            //now publish application bundle to the target hosting platform using the information provided in `target`
+
+        }
+    }
+
+}
+
+export { module }
