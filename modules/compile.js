3b212f67add03096213f0b0d5dec0d43e46a8ec3
David Pitman <david.pitman@cloudouble.com>
2024-08-29 17:07:53 +1000

    optimizing for 2.0.0 release

--- modules/compile.js
+++ modules/compile.js
@@ -234,6 +234,64 @@ const nativeElementsMap = {
             return Array.from(new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode(str)))).map(b => b.toString(16).padStart(2, '0')).join('')
         }
     },
+    getStateGroup: {
+        value: function (expression, typeDefault = 'cell', element) {
+            const parseOnly = !(element instanceof HTMLElement)
+            let group, shape
+            if (!parseOnly) element = this.app.components.virtuals.get(element) ?? element
+            const canonicalizeName = (name) => {
+                let type
+                switch (name[0]) {
+                    case '@': type = 'field'; break
+                    case '#': type = 'cell'; break
+                    default: type = typeDefault
+                }
+                const modeFlag = name[name.length - 1],
+                    mode = modeFlag === '!' ? 'force' : ((modeFlag === '?') ? 'silent' : undefined)
+                if (mode) name = name.slice(0, -1).trim()
+                return { name: name, mode, type }
+            }, getStateTarget = parseOnly ? undefined : (name, mode, type) => {
+                switch (type) {
+                    case 'cell':
+                        return { cell: (new this.Cell(name)), type, mode }
+                    case 'field':
+                        return { field: (new this.Field(name, undefined, element)), type, mode }
+                }
+            }
+            switch (expression[0]) {
+                case '{':
+                    group = {}
+                    shape = 'object'
+                    for (const pair of expression.slice(1, -1).trim().split(',')) {
+                        let [key, rawName] = pair.trim().split(':').map(s => s.trim())
+                        if (!rawName) rawName = key
+                        const { name, mode, type } = canonicalizeName(rawName)
+                        if (mode) key = key.slice(0, -1)
+                        group[key] = { name, mode, type }
+                        if (!parseOnly) group[key][type] = getStateTarget(name, mode, type)
+                    }
+                    break
+                case '[':
+                    group = []
+                    shape = 'array'
+                    for (let t of expression.slice(1, -1).split(',')) {
+                        t = t.trim()
+                        if (!t) continue
+                        const { name, mode, type } = canonicalizeName(t), index = group.push({ name, mode, type }) - 1
+                        if (!parseOnly) group[index][type] = getStateTarget(name, mode, type)
+                    }
+                    break
+                default:
+                    shape = 'single'
+                    expression = expression.trim()
+                    if (!expression) return
+                    group = canonicalizeName(expression)
+                    if (!parseOnly) group = getStateTarget(group.name, group.mode, group.type)
+            }
+            if (parseOnly) return { group, shape }
+            return group
+        }
+    },
     toBase32: {
         value: function (buffer) {
             const alphabet = 'abcdefghijklmnopqrstuvwxyz234567', size = buffer.length
