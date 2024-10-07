7607ed265f3b4bae0327085fc917d412ffade7c7
David Pitman <david.pitman@cloudouble.com>
2024-10-07 19:30:37 +1000

    optimizing for 2.0.0 release

--- element.js
+++ element.js
@@ -101,20 +101,15 @@ const ElementHTML = Object.defineProperties({}, {
                         for (const t of types) if (this.isWrappedVariable(t.name)) promises.push(this.resolveUnit(this.resolveVariable(t.name, valueEnvelope, { wrapped }), 'type'))
                         await Promise.all(promises)
                         let pass
-                        switch (mode) {
-                            case 'any':
-                                for (const { if: ifMode, name } of types) if (pass = (ifMode === (await this.runUnit(name, 'type', value)))) break
-                                break
-                            case 'all':
-                                for (const { if: ifMode, name } of types) if (!(pass = (ifMode === (await this.runUnit(name, 'type', value))))) break
-                                break
-                            case 'info':
-                                pass = true
-                                const validation = {}, promises = []
-                                for (const { name } of types) promises.push(this.runUnit(name, 'type', value, true).then(r => validation[name] = r))
-                                await Promise.all(promises)
-                                return { value, validation }
+                        if (mode === 'info') {
+                            pass = true
+                            const validation = {}, promises = []
+                            for (const { name } of types) promises.push(this.runUnit(name, 'type', value, true).then(r => validation[name] = r))
+                            await Promise.all(promises)
+                            return { value, validation }
                         }
+                        const [any, all] = [mode === 'any', mode === 'all']
+                        for (const { if: ifMode, name } of types) if (pass = (ifMode === (await this.runUnit(name, 'type', value)))) { if (any) break; } else if (all) break
                         if (pass) return value
                     },
                     binder: async function (container, position, envelope) {
@@ -126,11 +121,7 @@ const ElementHTML = Object.defineProperties({}, {
                     name: 'selector',
                     handler: async function (container, position, envelope, value) {
                         const { descriptor } = envelope, { scope, selector } = descriptor
-                        if (value != undefined) {
-                            const target = this.resolveSelector(selector, scope)
-                            if (Array.isArray(target)) for (const t of target) this.render(t, value)
-                            else if (target) this.render(target, value)
-                        }
+                        if (value != undefined) for (const t of ([].concat(this.resolveSelector(selector, scope)))) this.render(t, value)
                         return value
                     },
                     binder: async function (container, position, envelope) {
