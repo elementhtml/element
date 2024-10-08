3cfdcca9f058a0e77d068ccf703fff235b967f50
David Pitman <david.pitman@cloudouble.com>
2024-10-08 15:02:56 +1000

    optimizing for 2.0.0 release

--- element.js
+++ element.js
@@ -369,30 +369,7 @@ const ElementHTML = Object.defineProperties({}, {
         }
     },
     isWrappedVariable: { enumerable: true, value: function (expression) { return ((expression[0] === '$') && (expression[1] === '{') && (expression.endsWith('}'))) } }, // optimal
-    parse: {
-        enumerable: true, value: async function (input, contentType) {
-            const inputIsResponse = (input instanceof Response), inputIsText = (typeof input === 'text')
-            if (!(inputIsResponse || inputIsText)) return input
-            let inputUrlExtension
-            if (!contentType && inputIsResponse) {
-                const serverContentType = input.headers.get('Content-Type')
-                if (serverContentType !== 'application/octet-stream') contentType = serverContentType || undefined
-                if (!contentType) {
-                    const inputUrlPathname = (new URL(input.url)).pathname, suffix = inputUrlPathname.includes('.') ? inputUrlPathname.split('.').pop() : undefined
-                    contentType = suffix ? this.sys.suffixContentTypeMap[suffix] : undefined
-                    if (contentType) inputUrlExtension = suffix
-                }
-                if (!contentType || (contentType === 'text/html') || (contentType === 'text/plain')) return await input.text()
-            }
-            if (!contentType) return input
-            if (contentType === 'application/json') return (input instanceof Response) ? await input.json() : JSON.parse(input)
-            let text = ((input instanceof Response) ? await input.text() : input).trim()
-            if (contentType === 'text/css') return await (new CSSStyleSheet()).replace(text)
-            if (contentType && contentType.includes('form')) return Object.fromEntries((new URLSearchParams(text)).entries())
-            const contentTypeTransformer = this.resolveUnit(contentType, 'transform') ?? (inputUrlExtension ? this.resolveUnit(inputUrlExtension, 'transform') : undefined)
-            if (contentTypeTransformer) return contentTypeTransformer.run(text)
-        }
-    },
+    parse: { enumerable: true, value: async function (input, contentType) { return this.runFragment('parse', input, contentType) } }, // optimal
     render: { // optimal
         enumerable: true, value: async function (element, data) {
             if (!(element instanceof HTMLElement)) return
