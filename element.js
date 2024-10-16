cc9c452da6ebcd18444bb3b4e736cb4a582fc2e7
David Pitman <david.pitman@cloudouble.com>
2024-10-16 15:31:43 +1000

    optimizing for 2.0.0 release

--- element.js
+++ element.js
@@ -79,13 +79,79 @@ const ElementHTML = Object.defineProperties({}, {
                         if (value != undefined) for (const t of ([].concat(await this.resolveSelector(selector, scope)))) this.render(t, value)
                         return value
                     },
-                    binder: async function (facet, position, envelope) { return (await this.runFragment('env/interpreters/selector')).binder.call(this, facet, position, envelope) },
+                    binder: async function (facet, position, envelope) {
+                        const { descriptor } = envelope, { signal } = descriptor, { scope: scopeStatement, selector: selectorStatement } = descriptor,
+                            scope = await this.resolveScope(scopeStatement, facet.root), { sys } = this, { defaultEventTypes } = sys
+                        if (!scope) return {}
+                        const bangIndex = selectorStatement.lastIndexOf('!')
+                        let selector = selectorStatement.trim(), eventList
+                        if ((bangIndex > selector.lastIndexOf(']')) && (bangIndex > selector.lastIndexOf(')')) && (bangIndex > selector.lastIndexOf('"')) && (bangIndex > selector.lastIndexOf("'")))
+                            [selector, eventList] = [selector.slice(0, bangIndex).trim(), selector.slice(bangIndex + 1).trim()]
+                        if (eventList) eventList = eventList.split(sys.regexp.commaSplitter).filter(Boolean)
+                        else {
+                            const [statementIndex, stepIndex] = position.split('-')
+                            if (!facet.statements?.[+statementIndex]?.steps[+stepIndex + 1]) return { selector, scope }
+                        }
+                        for (let eventName of eventList ?? Array.from(new Set(Object.values(defaultEventTypes).concat(['click'])))) {
+                            const enSlice3 = eventName.slice(-3), keepDefault = enSlice3.includes('+'), exactMatch = enSlice3.includes('='), once = enSlice3.includes('-')
+                            for (const [v, r] of [[keepDefault, '+'], [exactMatch, '='], [once, '-']]) if (v) eventName = eventName.replace(r, '')
+                            scope.addEventListener(eventName, event => {
+                                let targetElement
+                                if (selector.endsWith('}') && selector.includes('{')) {
+                                    targetElement = this.resolveSelector(selector, scope)
+                                    if (!targetElement || (Array.isArray(targetElement) && !targetElement.length)) return
+                                } else if (selector[0] === '$') {
+                                    if (selector.length === 1) return
+                                    targetElement = exactMatch ? event.target : event.target.closest(selector)
+                                    if (!targetElement.matches(this.buildCatchallSelector(selector))) return
+                                } else if (selector && exactMatch && !event.target.matches(selector)) return
+                                targetElement ??= (exactMatch ? event.target : event.target.closest(selector))
+                                if (!targetElement) return
+                                if (!eventList && (event.type !== (targetElement.constructor.events?.default ?? defaultEventTypes[targetElement.tagName.toLowerCase()] ?? 'click'))) return
+                                if (!keepDefault) event.preventDefault()
+                                facet.eventTarget.dispatchEvent(new CustomEvent(`done-${position}`, { detail: this.flatten(targetElement, undefined, event) }))
+                            }, { signal, once })
+                        }
+                        return { selector, scope }
+                    },
                     parser: async function (expression) { return { signal: true, ...(await this.resolveScopedSelector(expression.slice(2, -1))) } }
                 }],
                 [/^[#@](?:[a-zA-Z0-9]+|[{][a-zA-Z0-9#@?!, ]*[}]|[\[][a-zA-Z0-9#@?!, ]*[\]])$/, {
                     name: 'state',
                     handler: async function (facet, position, envelope, value) { return (await this.runFragment('env/interpreters/state')).handler.call(this, facet, position, envelope, value) },
-                    binder: async function (facet, position, envelope) { return (await this.runFragment('env/interpreters/state')).binder.call(this, facet, position, envelope) },
+                    binder: async function (facet, position, envelope) {
+                        const { descriptor } = envelope, { signal, shape } = descriptor, items = []
+                        let { target } = descriptor, getReturnValue
+                        switch (shape) {
+                            case 'single':
+                                const { type, name } = target
+                                target[type] = type === 'field' ? (new this.Field(name, facet)) : (new this.Cell(name))
+                                getReturnValue = () => target[type].get()
+                                items.push(target)
+                                break
+                            case 'array':
+                                for (const t of target) (items[items.length] = t)[t.type] = t.type === 'field' ? (new this.Field(t.name, facet)) : (new this.Cell(t.name))
+                                getReturnValue = (r = [], l) => {
+                                    for (const t of target) if ((r[l ??= r.length] = t[t.type].get()) === undefined) return
+                                    return r
+                                }
+                                break
+                            case 'object':
+                                if (Array.isArray(target)) target = Object.fromEntries(target)
+                                for (const t of Object.values(target)) (items[items.length] = t)[t.type] = t.type === 'field' ? (new this.Field(t.name, facet)) : (new this.Cell(t.name))
+                                getReturnValue = (r = {}, tk) => {
+                                    for (const k in target) if ((r[k] = (tk = target[k])[tk.type].get()) === undefined) return
+                                    return r
+                                }
+                        }
+                        for (const item of items) {
+                            item[item.type].eventTarget.addEventListener('change', () => {
+                                const detail = getReturnValue()
+                                if (detail !== undefined) facet.eventTarget.dispatchEvent(new CustomEvent(`done-${position}`, { detail }))
+                            }, { signal })
+                        }
+                        return { getReturnValue, target }
+                    },
                     parser: async function (expression) { return (await this.runFragment('env/interpreters/state')).parser.call(this, expression) }
                 }],
                 [/^!\`[^`]+(\|[^`]+)?\`$/, {
--- fragments/env/interpreters/state.js
+++ fragments/env/interpreters/state.js
@@ -8,39 +8,6 @@ export default {
             case 'object': if (value instanceof Object) for (const k in value) if (value[k] !== undefined) if (k in target) target[k][target[k].type].set(value[k], target[k].mode)
         }
     },
-    binder: async function (facet, position, envelope) {
-        const { descriptor } = envelope, { signal, shape } = descriptor, items = []
-        let { target } = descriptor, getReturnValue
-        switch (shape) {
-            case 'single':
-                const { type, name } = target
-                target[type] = type === 'field' ? (new this.Field(name, facet)) : (new this.Cell(name))
-                getReturnValue = () => target[type].get()
-                items.push(target)
-                break
-            case 'array':
-                for (const t of target) (items[items.length] = t)[t.type] = t.type === 'field' ? (new this.Field(t.name, facet)) : (new this.Cell(t.name))
-                getReturnValue = (r = [], l) => {
-                    for (const t of target) if ((r[l ??= r.length] = t[t.type].get()) === undefined) return
-                    return r
-                }
-                break
-            case 'object':
-                if (Array.isArray(target)) target = Object.fromEntries(target)
-                for (const t of Object.values(target)) (items[items.length] = t)[t.type] = t.type === 'field' ? (new this.Field(t.name, facet)) : (new this.Cell(t.name))
-                getReturnValue = (r = {}, tk) => {
-                    for (const k in target) if ((r[k] = (tk = target[k])[tk.type].get()) === undefined) return
-                    return r
-                }
-        }
-        for (const item of items) {
-            item[item.type].eventTarget.addEventListener('change', () => {
-                const detail = getReturnValue()
-                if (detail !== undefined) facet.eventTarget.dispatchEvent(new CustomEvent(`done-${position}`, { detail }))
-            }, { signal })
-        }
-        return { getReturnValue, target }
-    },
     parser: async function (expression) {
         expression = expression.trim()
         const typeDefault = expression[0] === '@' ? 'field' : 'cell'
