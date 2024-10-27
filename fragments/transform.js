cc8d47e5fde54f9b920fb135564c370c1739a588
David Pitman <david.pitman@cloudouble.com>
2024-10-27 18:25:50 +1000

    optimizing for 2.0.0 release

--- fragments/transform.js
+++ fragments/transform.js
@@ -3,10 +3,7 @@ export default {
         const { E } = this.constructor, state = { ...this.pipelineState, ...(envelope.state ?? {}) }
         for (const k of state) if (E.isWrappedVariable(k)) state[k] = E.resolveVariable(state[k], envelope, { wrapped: true })
         const pipelineEnvelope = Object.freeze({ ...envelope, state })
-        if (this.steps instanceof Proxy) {
-            step ??= '*'
-            return this.steps[step].call(E, input, args, pipelineEnvelope, this)
-        }
+        if (this.steps instanceof Proxy) return this.steps[step ?? '*'].call(E, input, args, pipelineEnvelope, this)
         if (step && this.steps.has(step)) return this.steps.get(step)?.call(E, input, args, pipelineEnvelope, this)
         let useSteps = step.includes(':') ? E.sliceAndStep(step, this.steps.values()) : this.steps.values()
         for (const step of useSteps) if ((input = await step.call(E, input, args, pipelineEnvelope, this)) === undefined) break
