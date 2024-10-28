c978099114a4c28eb50df71fa2433c1687bb93da
David Pitman <david.pitman@cloudouble.com>
2024-10-28 15:29:16 +1000

    optimizing for 2.0.0 release

--- fragments/env/interpreters/transform.js
+++ fragments/env/interpreters/transform.js
@@ -1,5 +1,5 @@
 export default async function (facet, position, envelope, value) {
-    const { descriptor } = envelope, { transform: transformKey, flag } = descriptor,
+    const { descriptor } = envelope, { transform: transformKey, step, flag } = descriptor,
         valueEnvelope = Object.freeze({ ...envelope, value }), transform = await this.resolveUnit(transformKey, 'transform')
     return transform?.run(value, valueEnvelope, facet, position, { step, flag })
 }
